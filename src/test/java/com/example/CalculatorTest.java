dad7dfca1564fad54ac18bac666dd72078d02c9c
bhavanav9105 <bhavanav9105@gmail.com>
2026-04-22 14:51:33 +0530

    Add files via upload

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalculatorTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    Calculator cal = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, cal.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(2, cal.subtract(5, 2));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(20, cal.multiply(4, 5));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(5, cal.divide(10, 2));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        assertThrows(ArithmeticException.class, () -> {
+            cal.divide(10, 0);
+        });
+    }
+}
